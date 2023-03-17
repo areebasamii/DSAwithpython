e8c44de9f04be04bd94362ee494a435dc370dbf8
areebasamii <reebasami14@gmail.com>
2023-03-17 12:43:38 +0500

    class

---
+++ list.py
@@ -0,0 +1,20 @@
+List1=[0,9,7,5,2,1]
+print(List1)
+
+List2=[[1,2],(1,2,3),4,5]
+print(List2)
+print(List2[0])
+print(List2[1])
+print(List2[2])
+print(List2[0][0])
+print(List2[1][2])
+
+List3=['Joseph','Glem','Sally']
+print(List3)
+
+List4=['aREEBA',6,9,[3,4],90,'SAMI']
+print(List4)
+
+List5=['areeba','female',20,'Rs and GIS','IST',[89,0,7]]
+print(List5)
+
